04d0c20cdd824b07adea96f91138748248c5c2ad
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2021-09-26 20:25:47 +0200

    Add files via upload

---
+++ Key.py
@@ -0,0 +1,10 @@
+key = {
+  "apiKey": "AIzaSyDy1ivi7AfXIusQcyfZFL82e08XuTQf2pQ",
+  "authDomain": "newsapi-89a0b.firebaseapp.com",
+  "databaseURL": "https://newsapi-89a0b-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "newsapi-89a0b",
+  "storageBucket": "newsapi-89a0b.appspot.com",
+  "messagingSenderId": "708033801353",
+  "appId": "1:708033801353:web:e20567d7222ac73d762a58",
+  "measurementId": "G-E62PR3VJTD"
+}
--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from kivy.metrics import dp, sp
 from kivy.core.clipboard import Clipboard
 from  kivy.uix.popup import Popup
 from  kivy.uix.label import Label
-#import kivymd
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.checkbox import CheckBox
 
 import random
-
 import requests
+
+from pyrebase import pyrebase
+from Key import key
 
 if kivy.utils.platform == 'android' :
 
@@ -27,6 +30,7 @@ KV = """
 
 Screen:
     FloatLayout:
+        id: base
         size_hint: (1,1)
         ScrollView:
             size_hint_y:0.9
@@ -40,6 +44,18 @@ Screen:
             pos_hint: {'x':0.9}
             text: "Refresh"
             on_press: app.build_feed()
+        Button:
+            id: sel_api
+            size_hint: 0.9,0.1
+            pos_hint: {'x':0}
+            text: "Default"
+            on_press: app.select_api()
+        FloatLayout:
+            id:popup
+            
+            size_hint: 1,1
+            
+
         
 
 
@@ -58,7 +74,37 @@ tmp = [{'title':'1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phase
     {'title':'10Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sapien arcu, egestas vel dictum nec, consequat ','img':'https://raw.githubusercontent.com/Qypol342/App_EsayReadApi/main/tmp_img.png'},
     {'title':'11Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sapien arcu, egestas vel dictum nec, consequat ','img':'https://raw.githubusercontent.com/Qypol342/App_EsayReadApi/main/tmp_img.png'}]
 """
-tmp = [{'title': 'Blue Origin, Rocket Lab, SpaceX, ULA win Space Force contracts for rocket technology projects', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/50219312327_6aff9d5883_5k-scaled.jpg'}, {'title': 'Inside Varda Space’s plans to revolutionize in-space manufacturing', 'img': 'https://www.nasaspaceflight.com/wp-content/uploads/2021/09/photon-orbit.jpg'}, {'title': 'UN secretary-general criticizes “billionaires joyriding to space”', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/unsecgen.jpg'}, {'title': 'Orbit Fab to launch propellant tanker to fuel satellites in geostationary orbit', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-23-at-3.49.32-PM.png'}, {'title': 'NASA seeks a new ride for astronauts to the Artemis launch pad', 'img': 'https://cdn.arstechnica.net/wp-content/uploads/2021/09/KSC-2011-4822_large.jpg'}, {'title': 'House committee presses NOAA on commercial weather data and space traffic management', 'img': 'https://spacenews.com/wp-content/uploads/2019/03/spire-lemur-2019.jpg'}, {'title': 'Rocket Report: Analyst dings Virgin Galactic, Astranis moves to Falcon Heavy', 'img': 'https://cdn.arstechnica.net/wp-content/uploads/2021/09/51494577685_5e3c633172_k.jpg'}, {'title': 'SpaceX’s next commercial Falcon Heavy launch to carry Astranis rideshare satellite', 'img': 'https://www.teslarati.com/wp-content/uploads/2019/04/Falcon-Heavy-Flight-2-prelaunch-041119-Pauline-Acalin-clouds-pano-1-c.jpg'}, {'title': 'Photos: Landsat 9 encapsulated inside Atlas payload shroud', 'img': 'https://mk0spaceflightnoa02a.kinstacdn.com/wp-content/uploads/2021/09/KSC-20210816-PH-RNB01_0070large-2.jpg'}, {'title': 'Northrop Grumman to launch new satellite-servicing robot aimed at commercial and government market', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/CV_Capture.4k-scaled.jpg'}]
+#tmp = [{'title': 'Blue Origin, Rocket Lab, SpaceX, ULA win Space Force contracts for rocket technology projects', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/50219312327_6aff9d5883_5k-scaled.jpg'}, {'title': 'Inside Varda Space’s plans to revolutionize in-space manufacturing', 'img': 'https://www.nasaspaceflight.com/wp-content/uploads/2021/09/photon-orbit.jpg'}, {'title': 'UN secretary-general criticizes “billionaires joyriding to space”', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/unsecgen.jpg'}, {'title': 'Orbit Fab to launch propellant tanker to fuel satellites in geostationary orbit', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-23-at-3.49.32-PM.png'}, {'title': 'NASA seeks a new ride for astronauts to the Artemis launch pad', 'img': 'https://cdn.arstechnica.net/wp-content/uploads/2021/09/KSC-2011-4822_large.jpg'}, {'title': 'House committee presses NOAA on commercial weather data and space traffic management', 'img': 'https://spacenews.com/wp-content/uploads/2019/03/spire-lemur-2019.jpg'}, {'title': 'Rocket Report: Analyst dings Virgin Galactic, Astranis moves to Falcon Heavy', 'img': 'https://cdn.arstechnica.net/wp-content/uploads/2021/09/51494577685_5e3c633172_k.jpg'}, {'title': 'SpaceX’s next commercial Falcon Heavy launch to carry Astranis rideshare satellite', 'img': 'https://www.teslarati.com/wp-content/uploads/2019/04/Falcon-Heavy-Flight-2-prelaunch-041119-Pauline-Acalin-clouds-pano-1-c.jpg'}, {'title': 'Photos: Landsat 9 encapsulated inside Atlas payload shroud', 'img': 'https://mk0spaceflightnoa02a.kinstacdn.com/wp-content/uploads/2021/09/KSC-20210816-PH-RNB01_0070large-2.jpg'}, {'title': 'Northrop Grumman to launch new satellite-servicing robot aimed at commercial and government market', 'img': 'https://spacenews.com/wp-content/uploads/2021/09/CV_Capture.4k-scaled.jpg'}]
+
+class API_Manager():
+    def __init__(self):
+        self.api_list = []
+        firebase = pyrebase.initialize_app(key)
+        self.db = firebase.database()
+
+        res = self.db.child("NewsAPI").get()
+
+        for i in res.val():
+            self.api_list.append(i)
+        self.s_a = self.api_list[0]
+
+    def get_api_list(self):
+        return self.api_list
+
+    def set_select_api(self,nb):
+        
+        self.s_a =self.api_list[nb]
+        
+
+    def get_select_api(self):
+        return self.s_a
+
+
+    def get_api(self):
+        print(self.s_a)
+
+        return  self.db.child("NewsAPI").child(self.s_a).get().val()
+
 
 
 
@@ -68,6 +114,13 @@ class MainApp(App):#MDApp
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.screen = Builder.load_string(KV)
+
+        self.AM = API_Manager()
+        self.screen.ids.sel_api.text = self.AM.get_select_api()
+
+
+
         self.max_length = 42
         self.font_ratio = 0.003
         if kivy.utils.platform == 'android' :
@@ -75,7 +128,47 @@ class MainApp(App):#MDApp
             request_permissions([Permission.WRITE_EXTERNAL_STORAGE,
                      Permission.READ_EXTERNAL_STORAGE])
 
-        self.screen = Builder.load_string(KV)
+        
+
+
+    def select_api(self):
+        G = GridLayout(cols=1,size_hint=(0.8,0.8),pos_hint={'x':0.1,'y':0.1})
+        """
+        GridLayout:
+                id: check
+                cols:2
+                size_hint: 1,1
+        """
+
+        back = Button(background_color=((0,0,0,0.7)),
+             size_hint=(1,1),background_normal= '')
+        back.bind(on_press=self.quit_select_api)
+        self.screen.ids.popup.add_widget(back)
+        
+        list_api = self.AM.get_api_list()
+        for i in list_api:
+
+            b = Button(text=i)
+            b.bind(on_press=self.checkbox_active)
+            
+            G.add_widget(b)
+
+
+
+        self.screen.ids.popup.add_widget(G)
+        pass
+
+    def checkbox_active(self,df):
+        
+        name = df.text
+        index = self.AM.get_api_list().index(name)
+        self.AM.set_select_api(index)
+        self.quit_select_api("")
+        self.build_feed()
+        
+
+    def quit_select_api(self,df):
+        self.screen.ids.popup.clear_widgets()
 
 
     def better_text(self, text):
@@ -132,6 +225,9 @@ class MainApp(App):#MDApp
         
 
     def build_feed(self):
+
+        tmp = self.AM.get_api()
+        print(tmp)
 
        
         print(Window.size)
